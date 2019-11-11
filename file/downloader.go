1ec904fe0934a5ee7ea9ad6abfc1224db5acc768
hetao19 <376852921@qq.com>
2019-11-11 14:12:29 +0800

    add downloader.go

---
+++ file/downloader.go
@@ -0,0 +1,26 @@
+package file
+
+import (
+	"io"
+	"net/http"
+	"os"
+)
+
+// Download http url conntent convert to file
+func Download(toFile, url string) error {
+	f, err := os.Create(toFile)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	_, err = io.Copy(f, resp.Body)
+	return err
+        // test
+}
